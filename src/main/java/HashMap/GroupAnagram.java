075273751edfd5175faee8e617ff0f7c884b0bbd
osamazafar128@csu.fullerton.edu <Bigbang@5670006>
2019-04-12 15:04:27 -0700

    checking new yml

--- src/main/java/HashMap/GroupAnagram.java
+++ src/main/java/HashMap/GroupAnagram.java
@@ -9,7 +9,27 @@ public class GroupAnagram
 	
 	public static ArrayList<ArrayList<String>> anagram(String [] str)
 	{
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		HashMap<String, ArrayList<String>> map = new HashMap<>();
 		
+		for(int i = 0; i < str.length; i++)
+		{
+			char [] ch = str[i].toCharArray();
+			Arrays.sort(ch);
+			String sorted = String.valueOf(ch);
+			
+			if(!map.containsKey(sorted))
+			{
+				ArrayList<String> temp = new ArrayList<String>();
+				map.put(sorted, temp);
+			}
+			map.get(sorted).add(str[i]);
+			
+		}
+		
+		for(ArrayList<String> r: map.values())
+			result.add(r);
+		return result;
 	}
 	
 	
